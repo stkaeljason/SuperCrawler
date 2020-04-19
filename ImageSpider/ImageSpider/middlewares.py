79966ca2bfd90b018031d826fcc3c0f3b01ff8f5
stkaeljason <pudge702@qq.com>
2020-04-19 14:49:07 +0800

    update

--- ImageSpider/ImageSpider/middlewares.py
+++ ImageSpider/ImageSpider/middlewares.py
@@ -125,10 +125,10 @@ class ImagespiderDownloaderMiddleware(object):
     def process_request(self, request, spider):
         # request.meta['proxy'] = "https://127.0.0.1:" +str(proxy_port)
         request.meta['download_timeout'] = 25
-        request.headers['user-agent'] = random.choice(agents)
         if 'ins_im' in spider.name:
             # if 'query_hash=' in request.url or 'explore/locations' in request.url:
             if 'https://www.instagram.com/' in request.url and 'login' not in request.url:
+                request.headers['user-agent'] = random.choice(agents)
                 cookiejar_dict = spider.cookies_dict
                 if list(cookiejar_dict):
                     count = 0
