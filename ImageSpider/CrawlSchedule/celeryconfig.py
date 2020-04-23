1bf637a50aadecbdeda75dc4d6e5ba465d9a4a94
jqy <pudge702@qq.com>
2020-04-23 16:48:26 +0800

    update

--- ImageSpider/CrawlSchedule/celeryconfig.py
+++ ImageSpider/CrawlSchedule/celeryconfig.py
@@ -9,7 +9,7 @@ CELERY_TASK_SERIALIZER = 'msgpack' # 任务序列化和反序列化使用msgpack
 
 CELERY_RESULT_SERIALIZER = 'json' # 读取任务结果一般性能要求不高，所以使用了可读性更好的JSON
 
-CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24 # 任务过期时间，不建议直接写86400，应该让这样的magic数字表述更明显
+CELERY_TASK_RESULT_EXPIRES = 60 # 任务过期时间，不建议直接写86400，应该让这样的magic数字表述更明显
 
 CELERY_ACCEPT_CONTENT = ['json', 'msgpack'] # 指定接受的内容类型
 CELERY_TIMEZONE = 'Asia/Shanghai'
