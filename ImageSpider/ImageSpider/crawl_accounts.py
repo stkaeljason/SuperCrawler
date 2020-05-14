4ee91292c9ae89173bdbfd660d6eecdf987525c4
jqy <pudge702@qq.com>
2020-05-14 15:37:12 +0800

    update

--- ImageSpider/ImageSpider/crawl_accounts.py
+++ ImageSpider/ImageSpider/crawl_accounts.py
@@ -1,39 +1,34 @@
 #coding:utf-8
 
 user_crawl_list=[
-# {
-#     'username': 'liaohuan00001liaohuan',
-#     'password': 'st123456',
-#     'queryParams': {},
-# },
-# {
-#     'username': 'cym1111chenyammei',
-#     'password': 'st123456',
-#     'queryParams': {},
-# },
-# {
-#     'username': 'tjl1111tangjili',
-#     'password': 'st123456',
-#     'queryParams': {},
-# },
 {
-    'username': 'werttyy555',
-    'password': 'st123456',
+    'username': 'fiejf30f90_jfl',
+#    'password': 'st123456',
+    'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589438485:AdFQAJ8do0ZFCtnB9qyCMUaLmOx3NmHDz/uXLs22SA4AXv4O+CJ8EdQziGL/VaPFU2qFJvU5c+Xu2JYAZ/9T0iN2a0H+SAGFFk0CLsg/dqvgvctfB+Ps5fQxsWDrUHS9CtlgNwrYeieokbry',
     'queryParams': {},
 },
 {
-    'username': 'qrrr88555',
-    'password': 'st123456',
+    'username': '9fjosssssssf',
+#    'password': 'st123456',
+    'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589438596:AdFQADRBqCphW/qIBumrQ7N2aNzaZTZRG8540TBGf631yrxoipkPrkjuWx6ENg/vXHukfamu5orYpHfXP2U8AJnvbR2ZjOwFUdxvBQZvcAp3bHVGOjFOSXxi141GbwLC7KMS2B6FjIghdUoK',
     'queryParams': {},
 },
 {
-    'username': 'opopp45677',
-    'password': 'st123456',
+    'username': '9fejlfdfdfd_0kk',
+   # 'password': 'st123456',
+    'enc_password': '%23PWD_INSTAGRAM_BROWSER%3A10%3A1589437868%3AAdFQAN%2F0MyCuUUoxq7uAkKAuxbpqqw2iaeZGNzyU3p7rXEED0xFtnoAFYXEjwGhUc6OspZkkoGOohBpLLcXNVJje0kEs%2Bwf5gcbBsqsvW4eAck6l0TrUH6mvpC%2FilobD9U9mlxjaNL4auBND',
     'queryParams': {},
 },
 {
-    'username': 'inisng7743',
-    'password': 'st123456',
+    'username': 's3f87hehfffff',
+#    'password': 'st123456',
+    'enc_password': '%23PWD_INSTAGRAM_BROWSER%3A10%3A1589437868%3AAdFQAN%2F0MyCuUUoxq7uAkKAuxbpqqw2iaeZGNzyU3p7rXEED0xFtnoAFYXEjwGhUc6OspZkkoGOohBpLLcXNVJje0kEs%2Bwf5gcbBsqsvW4eAck6l0TrUH6mvpC%2FilobD9U9mlxjaNL4auBND',
+    'queryParams': {},
+},
+{
+    'username': '8jfieijf_0fkk',
+    #'password': 'st123456',
+    'enc_password':'#PWD_INSTAGRAM_BROWSER:10:1589438342:AdFQAEOsZ1XYLZBBqYgo08IXionnIR0x340X8E8OrFGNt8xAZc+PuAmgZIScuX8ZK9mi7fh3F7S+yd+SRwGtw7kVJNxqNvAQpC/0vmXeJT73wGgjgYUE3WbpaP19g3ahHXRA3B9U6pTXYdKA',
     'queryParams': {},
 },
 ]
