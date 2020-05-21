e08699319532f901cfcea7be1b1715a272767b5d
jqy <pudge702@qq.com>
2020-05-21 16:04:54 +0800

    update

--- ImageSpider/ImageSpider/crawl_accounts.py
+++ ImageSpider/ImageSpider/crawl_accounts.py
@@ -2,35 +2,35 @@
 
 user_crawl_list=[
 {
-    'username': 'fiejf30f90_jfl',
+    'username': 'f39efh9hll',
 #    'password': 'st123456',
     'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589438485:AdFQAJ8do0ZFCtnB9qyCMUaLmOx3NmHDz/uXLs22SA4AXv4O+CJ8EdQziGL/VaPFU2qFJvU5c+Xu2JYAZ/9T0iN2a0H+SAGFFk0CLsg/dqvgvctfB+Ps5fQxsWDrUHS9CtlgNwrYeieokbry',
     'queryParams': {},
 },
 {
-    'username': '9fjosssssssf',
+    'username': 'fje930jfj_0ojop',
 #    'password': 'st123456',
     'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589438596:AdFQADRBqCphW/qIBumrQ7N2aNzaZTZRG8540TBGf631yrxoipkPrkjuWx6ENg/vXHukfamu5orYpHfXP2U8AJnvbR2ZjOwFUdxvBQZvcAp3bHVGOjFOSXxi141GbwLC7KMS2B6FjIghdUoK',
     'queryParams': {},
 },
 {
-    'username': '9fejlfdfdfd_0kk',
+    'username': '9feje023jfe00',
    # 'password': 'st123456',
     'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589508149:AZpQAJMe38O08Oe/qXYeQlubtl/kE1ot8P5G4xhX08fC4cQnL3SBETlpnYgGdTmV7HZyHUzD+BUK0cPb9Zl0CeyYwoqHeaf+jYxJQgP3hpTyJP0kYDJeOaLFxJ+ttmd1UjiD5tETFx376Fwm',
     'queryParams': {},
 },
 {
-    'username': 's3f87hehfffff',
+    'username': 'f9999j0jjj_09i',
 #    'password': 'st123456',
     'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1589508248:AZpQAASM1sJk6RaZqL7Jrt3IMz7BzBLURbrcddERhKbKmZ0gW6Yuufxhl5DKKgree9GeeQzX16Bt05fI/PFa/iMudThb3KtdRKr1bTNlB3ggpntpjcADGW5eRmhssYFPXgteQDZCH1p71SEo',
     'queryParams': {},
 },
-{
-    'username': '8jfieijf_0fkk',
-    #'password': 'st123456',
-    'enc_password':'#PWD_INSTAGRAM_BROWSER:10:1589438342:AdFQAEOsZ1XYLZBBqYgo08IXionnIR0x340X8E8OrFGNt8xAZc+PuAmgZIScuX8ZK9mi7fh3F7S+yd+SRwGtw7kVJNxqNvAQpC/0vmXeJT73wGgjgYUE3WbpaP19g3ahHXRA3B9U6pTXYdKA',
-    'queryParams': {},
-},
+# {
+#     'username': '8jfieijf_0fkk',
+#     #'password': 'st123456',
+#     'enc_password':'#PWD_INSTAGRAM_BROWSER:10:1589438342:AdFQAEOsZ1XYLZBBqYgo08IXionnIR0x340X8E8OrFGNt8xAZc+PuAmgZIScuX8ZK9mi7fh3F7S+yd+SRwGtw7kVJNxqNvAQpC/0vmXeJT73wGgjgYUE3WbpaP19g3ahHXRA3B9U6pTXYdKA',
+#     'queryParams': {},
+# },
 ]
 tag_accounts = {'tag0':[
 {
